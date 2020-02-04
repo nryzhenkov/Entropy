44915a82436a1baec1c68af604c2286375cb3351
nryzhenkov <nryzhenkov@yandex.ru>
2020-02-04 21:52:34 +0300

    Add StringAnalyzer

---
+++ EntropyLib/StringAnalyzer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EntropyLib
+{
+    public class StringAnalyzer
+    {
+        Dictionary<char, string> rule = new Dictionary<char, string>();
+
+        public string FromCharToCode(char ch)
+        {
+            return rule[ch];
+        }
+        public void MakeRule(string str)
+        {
+            int code = 0;
+            for(int i = 0; i < str.Length; i++)
+            {
+                if (rule.ContainsKey(str[i]))
+                {
+                    rule.Add(str[i], Convert.ToString(code, 2));
+                    code++;
+                }
+            }
+        }
+    }
+}
