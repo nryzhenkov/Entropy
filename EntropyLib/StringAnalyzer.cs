003f435b77f6157bd30bcfb9e3c06b15e87b960f
nryzhenkov <nryzhenkov@yandex.ru>
2020-02-05 21:23:27 +0300

    Add function FromStrToCode

--- EntropyLib/StringAnalyzer.cs
+++ EntropyLib/StringAnalyzer.cs
@@ -1,12 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
 
+
 namespace EntropyLib
 {
     public class StringAnalyzer
     {
         Dictionary<char, string> rule = new Dictionary<char, string>();
-
+        
+        public string ToFormat(char ch)
+        {
+            string str = rule[ch];
+            string result = "";
+            int numberDigits = (int)Math.Ceiling(Math.Log2(rule.Count));
+            if (str.Length != numberDigits)
+            {
+                for (int i = 0; i < numberDigits - str.Length; i++)
+                    result += "0";
+            }
+            result += str;
+            return result;
+        }
+        public string FromStrToCode(string str)
+        {
+            string result = "";
+            for(int i = 0; i < str.Length; i++)
+            {
+                result += ToFormat(str[i]);
+            }
+            return result;
+        }
         public string FromCharToCode(char ch)
         {
             return rule[ch];
